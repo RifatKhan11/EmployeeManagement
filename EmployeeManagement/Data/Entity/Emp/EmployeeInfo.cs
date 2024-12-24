f0af346faa6a5bbafdf0542dfaa6f778cd3a3530
RifatKhan11 <rifat.v.gpi@gmail.com>
2024-12-24 17:10:51 +0600

    gg

--- EmployeeManagement/Data/Entity/Emp/EmployeeInfo.cs
+++ EmployeeManagement/Data/Entity/Emp/EmployeeInfo.cs
@@ -1,14 +1,22 @@
 ﻿using EmployeeManagement.Data.Entity.MasterData;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EmployeeManagement.Data.Entity.Emp
 {
     public class EmployeeInfo: Base
     {
+        [Column(TypeName = "NVARCHAR(200)")]
         public string name { get; set; }
+
+        [Column(TypeName = "NVARCHAR(150)")]
         public string email { get; set; }
+
+        [Column(TypeName = "NVARCHAR(20)")]
         public string phoneNumber { get; set; }
         public int? departmentId { get; set; }
         public DepartmentInfo department { get; set; }
+
+        [Column(TypeName = "NVARCHAR(100)")]
         public string position { get; set; }
         public DateTime? joiningDate { get; set; }
     }
---
+++ EmployeeManagement/Migrations/20241224105504_typeAdd.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.Migrations
+{
+    /// <inheritdoc />
+    public partial class typeAdd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "position",
+                table: "Employees",
+                type: "NVARCHAR(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phoneNumber",
+                table: "Employees",
+                type: "NVARCHAR(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "Employees",
+                type: "NVARCHAR(200)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Employees",
+                type: "NVARCHAR(150)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "position",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "phoneNumber",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(20)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "name",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(200)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "email",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(150)",
+                oldNullable: true);
+        }
+    }
+}
